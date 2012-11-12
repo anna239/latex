7ed213d6c5c9845237dc6c91d13944bb7e601d72
Evgeniy Krasko <krasko.evgeniy@gmail.com>
2012-11-13 00:23:43 +0400

    small bug fix in extractor

--- src/latex/formulaextractor/FormulaExtractor.scala
+++ src/latex/formulaextractor/FormulaExtractor.scala
@@ -3,7 +3,7 @@ package latex.formulaextractor
 
 class FormulaExtractor {
   def extract(doc: String): Array[String] = {
-    doc.replaceAll("\\\\[$]", "").split("([$]{2})|[$]").zipWithIndex.filter {
+    (" " + doc).replaceAll("\\\\[$]", "").split("([$]{2})|[$]").zipWithIndex.filter {
       case (s, i) => i % 2 == 1
     }.map {
       case (s, i) => s
