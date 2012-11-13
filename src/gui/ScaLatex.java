9de20efb8e79673c0d1d1b30bae4d8f21ad1f0b0
Evgeniy Krasko <krasko.evgeniy@gmail.com>
2012-11-13 16:57:27 +0400

    drag-and-drop added

--- src/gui/ScaLatex.java
+++ src/gui/ScaLatex.java
@@ -4,7 +4,13 @@ import latex.formulaextractor.FormulaExtractor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,38 +19,85 @@ import java.io.File;
  * Time: 8:34 PM
  */
 public class ScaLatex {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         ScaLatexFrame frame = new ScaLatexFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 }
 
-class ScaLatexFrame extends JFrame
-{
-    public ScaLatexFrame()
-    {
+class ScaLatexFrame extends JFrame {
+    public ScaLatexFrame() {
         setTitle("ScaLaTex");
-        JFileChooser chooser = new JFileChooser();
-        chooser.setCurrentDirectory(new File("."));
-        chooser.showOpenDialog(this);
-        String filename = chooser.getSelectedFile().getAbsolutePath();
 
-        formulae = new FormulaExtractor().extract(new File(filename));
+        JMenuBar bar = new JMenuBar();
+
+        JMenu menu = new JMenu("File");
+
+        JMenuItem item = new JMenuItem("Open TeX file");
+
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser();
+                chooser.setCurrentDirectory(new File("."));
+                chooser.showOpenDialog(ScaLatexFrame.this);
+                if (chooser.getSelectedFile() == null) return;
+                String filename = chooser.getSelectedFile().getAbsolutePath();
+                addContent(filename);
+            }
+        });
+
+        setTransferHandler(new FileDropHandler());
+
+        setJMenuBar(bar);
+        bar.add(menu);
+        menu.add(item);
+        setMinimumSize(new Dimension(400, 600));
+    }
+
+
+    private void addContent(String filename) {
+        getContentPane().removeAll();
+
+        String[] formulae = new FormulaExtractor().extract(new File(filename));
 //        formulae = new String[20];
 //        for (int i = 0; i < formulae.length; i++) formulae[i] = "a + b = ";
         ScaLatexPanel panel = new ScaLatexPanel(formulae);
-        Container contentPane = getContentPane();
 
         JScrollPane pane = new JScrollPane(panel);
 
-        contentPane.add(pane);
+        getContentPane().add(pane);
 
         pack();
     }
 
-    private String[] formulae;
+    class FileDropHandler extends TransferHandler {
+        public boolean canImport(TransferSupport supp) {
+            return supp.isDrop() && supp.isDataFlavorSupported(DataFlavor.javaFileListFlavor);
+        }
+
+        public boolean importData(TransferSupport supp) {
+            if (!canImport(supp)) {
+                return false;
+            }
+            Transferable t = supp.getTransferable();
+            try {
+                Object data = t.getTransferData(DataFlavor.javaFileListFlavor);
+                java.util.List fileList = (java.util.List) data;
+                for (Object file : fileList) {
+                    if (file instanceof File) {
+                        addContent(((File) file).getAbsolutePath());
+                    }
+                }
+            } catch (UnsupportedFlavorException e) {
+                return false;
+            } catch (IOException e) {
+                return false;
+            }
+            return true;
+        }
+    }
 }
 
 
